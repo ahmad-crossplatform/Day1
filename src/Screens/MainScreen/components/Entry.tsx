5409333f1d27b72c2684449a59f0d6adfda31b54
Ahmad ElMadi <ahmad@crossplatform.se>
2021-11-04 11:01:51 +0100

    - UseEffect - Async Storage - Custom Fonts

--- src/Screens/MainScreen/components/Entry.tsx
+++ src/Screens/MainScreen/components/Entry.tsx
@@ -1,16 +1,20 @@
 import React from 'react' ; 
 import { View,Text,TextInput,StyleSheet } from 'react-native';
-
+import { useFonts } from 'expo-font';
 interface IEntry {
     label: string, 
     isPassword: boolean
+    OnTextChanged?: (text: string) => void;
+    defaultValue?: string;
 }
 
 export const Entry : React.FC<IEntry> = (props) => {
+  const [fontsLoaded] = useFonts({'CrazyFont':require('../../../../assets/fonts/CevicheOne-Regular.ttf')});
     return (          
     <View style={styles.usernameContainer}>
-        <Text style={styles.label}>{props.label}</Text>
-        <TextInput secureTextEntry={props.isPassword} style={styles.username}/>  
+      {fontsLoaded && <Text style={styles.label}>{props.label}</Text>}
+        
+        <TextInput defaultValue={props.defaultValue} onChangeText={props.OnTextChanged} secureTextEntry={props.isPassword} style={styles.username}/>  
       </View>)
 }
 
@@ -33,7 +37,8 @@ const styles = StyleSheet.create(
         
           },
           label:{
-            fontSize: 20,
+            fontFamily:'CrazyFont', 
+            fontSize: 50,
             fontWeight:'bold',
             margin:5,
           },
